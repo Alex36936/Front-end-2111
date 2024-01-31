1ff3c996a514a8ad2d0c42638884aa64fffaccf4
Alexa36936 <vakulenkosasha1995@gmail.com>
2024-01-31 08:34:49 +0100

    My first JS code

---
+++ 01/main.js
@@ -0,0 +1,18 @@
+//First Task
+let userNickname='Nick';
+let userScore=25;
+
+//Second Task
+let price = prompt('Ціна покупки');
+let quantity = prompt('кількість продуктів');
+
+console.log('price', price);
+console.log('quantity', quantity);
+
+let summOperation = (+price+ +quantity);
+console.log(summOperation);
+
+//Hard Task
+let sale = ((summOperation*5)/100);
+console.log(sale);
+
